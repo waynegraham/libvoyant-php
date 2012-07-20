893b3c0fc4ae6c74454f13b19dd0d29741cf44ed
Wayne Graham <wayne.graham@virginia.edu>
2012-07-20 15:39:06 +0200

    API Documentation
    Adds documentation for methods

--- VoyeurService.php
+++ VoyeurService.php
@@ -1,26 +1,64 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+/**
+ * VoyeurService 
+ *
+ * @category  Web_Services
+ * @package   Voyeur
+ * @author    Wayne Graham <wayne.graham@gmail.com>
+ * @author    Eric Rochester <>
+ * @copyright 2012 For realz
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache 2.0
+ * @link      https://github.com/waynegraham/libvoyant-php
+ */
 class VoyeurService
 {
-  protected $_host, $_port, $_path;
-  
-  public function __construct($host = 'localhost', $port = '8080', $path = '/')
+    protected $host, $port, $path;
+
+    /**
+     * Constructor. All parameters are optional and will use default 
+     * values if not specified
+     *
+     * @param string $host Hostname
+     * @param string $port Port
+     * @param string $path Path
+     *
+     * @return void
+     */
+    public function __construct($host = 'voyeurtools.org', $port = '80', $path = '/trombone')
     {
         $this->setHost($host);
 
+        return $this;
+
     }
 
-  public function setHost($host)
-  {
-    if (empty($host)) {
-      throw new Voyeur_Service_InvalidArgumentException('Host parameter cannot be empty');
+    /**
+     * Set the host to be used. If empty, throws an exception
+     *
+     * @param string $host Host to use
+     *
+     * @throws InvalidArgumentException If the host parametet is empty
+     *
+     * @return void
+     */
+    public function setHost($host)
+    {
+        if (empty($host)) {
+            throw new InvalidArgumentException('Host parameter cannot be empty');
+        }
+
+        $this->host = $host;
+    }  
+
+    /**
+     * Return the host
+     *
+     * @return string 
+     */
+    public function getHost()
+    {
+        return $this->host;
     }
-
-    $this->_host = $host;
-  }  
-
-  public function getHost()
-  {
-    return $this->_host;
-  }
 }
--- tests/VoyeurServiceTest.php
+++ tests/VoyeurServiceTest.php
@@ -1,15 +1,50 @@
 <?php
-
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 class Voyeur_ServiceTest extends PHPUnit_Framework_TestCase
 {
+    public function testConstructorDefaultArguments()
+    {
+        $fixture = new VoyeurService();
+        $this->assertInstanceOf('VoyeurService', $fixture);
+    }
+
+    public function testGetHostWithDefaultConstructor()
+    {
+        $fixture = new VoyeurService();
+        $host = $fixture->getHost();
+
+        $this->assertEquals('voyeurtools.org', $host);
+    }
+
     public function testSetHost()
     {
-      $host = "localhost";
-      $fixture = new VoyeurService();
+        $host = "localhost";
+        $fixture = new VoyeurService();
 
-      $fixture->setHost($host);
+        $fixture->setHost($host);
 
-      $this->assertEquals($host, $fixture->getHost());
+        $this->assertEquals($host, $fixture->getHost());
 
+    }
+
+    public function testSetEmptyHost()
+    {
+        $this->setExpectedException(
+            'InvalidArgumentException',
+            'Host parameter cannot be empty'
+        );
+
+        $fixture = new VoyeurService();
+        $fixture->setHost('');
+    }
+
+    public function testSetHostWithConstructor()
+    {
+        $host = 'example.com';
+
+        $fixture = new VoyeurService($host);
+
+
+        $this->assertEquals($host, $fixture->getHost());
     }
 }
