0ade93bde963540720fe16d22d5fdf9c7b9c181f
Wayne Graham <wayne.graham@virginia.edu>
2012-07-20 18:28:03 +0200

    Fix issue including interface

--- HttpTransport/Abstract.php
+++ HttpTransport/Abstract.php
@@ -1,6 +1,8 @@
 <?php
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
+require_once dirname(__FILE__) . '/Interface.php';
+
 abstract class Voyeur_HttpTransport_Abstract implements Voyeur_HttpTransport_Interface
 {
     private $_defaultTimeout = false;
